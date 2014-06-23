80d916af3ec015bd6066648788861c29bf1547be
Fara <faraabdullaev@gmail.com>
2014-06-23 16:26:22 +0500

    add feed forward algo

--- protected/components/Archer/NNetwork.php
+++ protected/components/Archer/NNetwork.php
@@ -2,6 +2,15 @@
 
 class NNetwork {
 
+	private $wordIds;
+	private $urlIds;
+	private $hiddenIds;
+	private $ai = [];
+	private $ah = [];
+	private $ao = [];
+	private $wi = [];
+	private $wo = [];
+
 	function __construct(){
 		return true;
 	}
@@ -50,4 +59,71 @@ class NNetwork {
 				$this->setStrength($hdID, $id, 1, 0.1);
 		}
 	}
+
+	function getAllHiddenIds($worddIds, $urlIds){
+		$l1 = [];
+		foreach( $worddIds as $id ){
+			$words = WordHd::model()->findAllByAttributes(['fromId'=>$id]);
+			foreach($words as $word)
+				$l1[ $word->toId ] = 1;
+		}
+		foreach( $urlIds as $id){
+			$urls = UrlHd::model()->findAllByAttributes(['toId'=>$id]);
+			foreach($urlIds as $url)
+				$l1[ $url->fromId ] = 1;
+		}
+		return array_keys($l1);
+	}
+
+	function setupNetwork($wordIds, $urlIds){
+		$this->wordIds = $wordIds;
+		$this->urlIds = $urlIds;
+		$this->hiddenIds = $this->getAllHiddenIds($wordIds, $urlIds);
+
+		//$this->ai = 1.0*count($this->wordIds);
+		//$this->ah = 1.0*count($this->hiddenIds);
+		//$this->ao = 1.0*count($this->urlIds);
+
+		/** Создаем матрицу весов */
+		$i = 0;
+		$j = $i;
+		foreach($this->wordIds as $wid){
+			foreach($this->hiddenIds as $hid){
+				$this->wi[$i][$j] = $this->getStrength($wid, $hid, 0);
+				$j++;
+			}
+			$j = 0;
+			$i++;
+		}
+		foreach($this->hiddenIds as $hid){
+			foreach($this->urlIds as $uid){
+				$this->wo[$i][$j] = $this->getStrength($hid, $uid, 1);
+				$j++;
+			}
+			$j = 0;
+			$i++;
+		}
+	}
+
+	function FeedForward(){
+		for($i=0; $i<count($this->wordIds); $i++) $this->ai[$i] = 1.0;
+		/** Возбуждение скрытых узлов */
+		for($j=0; $j<count($this->hiddenIds); $j++){
+			$sum = 0.0;
+			for($i=0; $i<count($this->wordIds); $i++){
+				$sum += $this->ai[$i] * $this->wi[$i][$j];
+			}
+			$this->ah[$j] = tanh($sum);
+		}
+		/** Возбуждение выходных узлов */
+		for($k=0; $k<count($this->urlIds); $k++){
+			$sum = 0.0;
+			for($j=0; $j<count($this->hiddenIds); $j++){
+				$sum += $this->ah[$j] * $this->wo[$j][$k];
+			}
+			$this->ao[$k] = tanh($sum);
+		}
+		return $this->ao;
+	}
+
 }
